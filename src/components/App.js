fa49d0eb98111f67086480c833ecc2971243dbdb
sanskarsahu <sanskarsahu1580@gmail.com>
2024-02-09 00:39:00 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import "./../styles/App.css";
 import Tabs from "./Tabs";
 
 const tabData = [
-  { title: "Tab  1", content: "This is the content for Tab 1." },
-  { title: "Tab  2", content: "This is the content for Tab 2." },
-  { title: "Tab  3", content: "This is the content for Tab 3." },
+  { title: "Tab 1", content: "This is the content for Tab 1." },
+  { title: "Tab 2", content: "This is the content for Tab 2." },
+  { title: "Tab 3", content: "This is the content for Tab 3." },
 ];
 
 const App = () => {
